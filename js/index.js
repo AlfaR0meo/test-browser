91148fcd39296d439b86b1a95b80bb9e92b7f33f
Deniz <80147573+AlfaR0meo@users.noreply.github.com>
2024-05-01 14:59:55 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const browserNameSpan = document.querySelector('.browser-name > span');
+
+function identifyUserBrowser() {
+    console.log(navigator);
+
+    const navigatorUserAgent = navigator.userAgent.toLowerCase();
+    console.log(navigatorUserAgent);
+
+    if (
+        navigatorUserAgent.includes('chrome') &&
+        !navigatorUserAgent.includes('edg') &&
+        !navigatorUserAgent.includes('opr')
+    ) {
+        browserNameSpan.textContent = 'Chrome';
+        return 'Chrome';
+    }
+    if (navigatorUserAgent.includes('firefox')) {
+        browserNameSpan.textContent = 'Firefox';
+        return 'Firefox';
+    }
+    if (navigatorUserAgent.includes('edg')) {
+        browserNameSpan.textContent = 'Edge';
+        return 'Edge';
+    }
+    if (navigatorUserAgent.includes('opr')) {
+        browserNameSpan.textContent = 'Opera';
+        return 'Opera';
+    }
+}
+identifyUserBrowser()
